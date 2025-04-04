223f0af0a334448cfa622a9c92524a79bbb58492
luis <luis.lozano.sanz43@gmail.com>
2025-04-04 09:23:16 +0200

    guardar cambio

--- EjercicioMatriz/matriz_itera.py
+++ EjercicioMatriz/matriz_itera.py
@@ -1,13 +1,12 @@
 def determinante(matriz):
-    if len(matriz) !=3 or len(matriz[0]!=3):
+    if len(matriz)!= 3 or len(matriz[0]!= 3):
         print("La matriz debe ser 3x3")
 
     a,b,c=matriz[0]
     d,e,f=matriz[1]
     g,h,i=matriz[2]
 
-matriz_3x3 = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+
+    
+
 ]
